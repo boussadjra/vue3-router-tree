4299bf21dfa8f20a9880ef49acd8bd88c0c2a57f
Brahim <cb_bousssadjra@esi.dz>
2020-10-03 20:17:31 +0100

    deploy to gh-pages

---
+++ dist/js/about.0965147c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),o={class:"about"},c=Object(e["f"])("h1",null,"This is an about page",-1);function s(n,t,a,s,u,b){return Object(e["o"])(),Object(e["d"])("div",o,[c])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.0965147c.js.map
